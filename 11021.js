43031cdea8ca986600f0e3329c4d8d5cd061276c
Hanbyoul <dytjqdla120@naver.com>
2023-02-12 21:57:33 +0900

    11021

--- 11021.js
+++ 11021.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const input = fs.readFileSync("ex.txt").toString().trim().split("\n");
+
 const count = input[0];
-let result = [];
 for (let i = 1; i <= count; i++) {
-  let line = input[i].split(" ").map((i) => +i);
-  result += line[0] + line[1] + "\n";
+  let tCase = `Case #${i}:`;
+  let line = input[i].split(" ").map((num) => +num);
+  console.log(tCase, line[0] + line[1]);
 }
-console.log(result);
