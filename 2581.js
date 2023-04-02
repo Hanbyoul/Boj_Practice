71a05b3532e75278357c7149553f988247389650
Hanbyoul <dytjqdla120@naver.com>
2023-04-02 22:45:17 +0900

    2581

--- 2581.js
+++ 2581.js
@@ -1,26 +1,39 @@
-// 60
-// 100
-
-//소수들의 합
-// 최소 소수값
-
-const [start, end] = require("fs")
+const [min, max] = require("fs")
   .readFileSync("ex.txt")
   .toString()
   .trim()
   .split("\n")
   .map(Number);
 
-console.log(start, end);
+let arr = Array.from({ length: max + 1 }).fill(true);
+let sum = 0;
+const sqrt = parseInt(Math.sqrt(max));
 
-// for (let i = 0; i < N; i++) {
-//   let result = [];
-//   for (let j = 1; j <= numbers[i]; j++) {
-//     if (!(numbers[i] % j)) {
-//       result.push(j);
-//     }
-//   }
-//   if (result.length === 2) {
-//     cnt++;
-//   }
-// }
+arr[0] = arr[1] = false;
+
+for (let i = 2; i <= sqrt; i++) {
+  if (arr[i]) {
+    for (let j = 2; i * j <= max; j++) {
+      arr[i * j] = false;
+    }
+  }
+}
+
+let primeNumber = arr
+  .map((num, index) => {
+    if (num) {
+      return (num = index);
+    }
+  })
+  .filter((i) => i !== undefined && i >= min && i <= max);
+
+for (let k = 0; k < primeNumber.length; k++) {
+  sum += primeNumber[k];
+}
+
+if (primeNumber.length >= 1) {
+  console.log(sum);
+  console.log(primeNumber[0]);
+} else {
+  console.log(-1);
+}
