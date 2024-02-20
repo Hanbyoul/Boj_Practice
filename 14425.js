af887eeac501d6c1c4bd7f5e8177ab62e522f449
Hanbyoul <dytjqdla120@naver.com>
2024-02-20 18:47:48 +0900

    14425

---
+++ 14425.js
@@ -0,0 +1,13 @@
+const [num, ...arr] = require("fs")
+  .readFileSync("ex.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [N] = num.split(" ").map(Number);
+
+const targetN = new Set(arr.splice(0, N));
+
+const result = [...arr].filter((v) => targetN.has(v));
+
+console.log(result.length);
