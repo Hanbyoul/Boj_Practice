c358659bf4fb10b5c429ac3f6c189ab7495f0581
Hanbyoul <dytjqdla120@naver.com>
2024-03-04 13:45:45 +0900

    1269

--- try.js
+++ try.js
@@ -1,20 +1,14 @@
-const [N, ...arr] = require("fs")
+const [n, a, b] = require("fs")
   .readFileSync("ex.txt")
   .toString()
   .trim()
-  .split("\n");
+  .split("\n")
+  .map((n) => n.split(" "));
 
-const set1 = new Set();
-const result = [];
+const [A, B] = n.map(Number);
+console.log(A + B); // 8
 
-const [targetN] = N.split(" ").map(Number);
+const setAB = new Set([...a, ...b]); // AUB (AB합집합)
+console.log(setAB.size); // 6
 
-for (let i = 0; i < arr.length; i++) {
-  if (i < targetN) {
-    set1.add(arr[i]);
-  } else {
-    if (set1.has(arr[i])) result.push(arr[i]);
-  }
-}
-
-console.log(result.length + "\n" + result.sort().join("\n"));
+console.log(setAB.size * 2 - (A + B));
